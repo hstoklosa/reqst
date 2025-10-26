c90d9198d3fd66be0dc07a64b5707c08def47b0c
imexotic <hubert.stoklosa23@gmail.com>
2025-10-26 20:59:46 +0000

    chore: remove unnecessary return statements from utility functions

--- reqst/utils.py
+++ reqst/utils.py
@@ -2,6 +2,6 @@ import sys
 
 
 def bout(x): sys.stdout.buffer.write(x)
-def out(x): return sys.stdout.write(str(x)+"\n")
-def err(x): return sys.stderr.write(str(x)+"\n")
+def out(x): sys.stdout.write(str(x)+"\n")
+def err(x): sys.stderr.write(str(x)+"\n")
 def by_key_lower(item: tuple): key, val = item; return key.lower()
